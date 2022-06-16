6a160d1c18c6c8146cea1baee21a75e702539139
Julia Conzatti <104903634+juliaconzatti@users.noreply.github.com>
2022-06-16 01:25:16 -0300

    clube Romance adicionado

--- clubeRomance.php
+++ clubeRomance.php
@@ -13,6 +13,8 @@
 </head>
 <body>
 
+<!--cada clube terá sua própria página-->
+
 <nav class="navbar" style="background-color: #180a25;" id="nav1">
         <div class="container-md"></div>
           <a class="navbar-brand" href="www.instagram.com"><img src="img/logoinstagram.png" width="20px" height="20px" class="d-inline-block align-text-top"></a>
@@ -27,13 +29,33 @@
         </div>
       </nav>
 
-<div class="d-grid gap-2 col-6 mx-auto">
-  <a href="atualizarLeitura.php"><button class="btn btn-primary" type="button">Fazer atualização de leitura</button></a>
+      <div class="container">
+  <div class="row">
+    <div class="col" id="divesq">
+      <h3 class="text-left">Livro do mês:</h3>
+          <a href="clubeRomance.php">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81AFV9waKfL.jpg" width="200px" height="300px">
+          </a>
+          <div id="btnBaixar">
+          <a href="livros/thelovehypothesis.pdf" download="livrodomes"><button class="btn btn-outline-dark" type="button">Fazer download</button></a>
+      </div>
+        </div>
+
+    <div class="col" id="divdir">
+<div>
+<h3 class="text-left">Descricão:</h3>
+<p class="text-break text-start">Praise for Tweet Cute “From meme wars to social media marketing, Lord accurately depicts various sides of today's online culture. 
+  Amid all the digital hoopla is an engaging story about family loyalty and pursuing one's own passions.</p>
+<div class="d-grid gap-2 col-6 mx-autot">
+  <a href="atualizarLeitura.php"><button class="btn btn-primary" type="button">Atualizar leitura</button></a>
   <a href="forum.php"><button class="btn btn-success" type="button">Acessar fórum</button></a>
   <a href="MUDAR ISSO"><button class="btn btn-danger" type="button">Sair deste clube</button></a>
 </div>
+</div>
+  </div>
+</div>
 <!--MUDAR ESSA ÚLTIMA PARTE DO BOTAO DE SAIR DO CLUBE E DEIXAR OS BOTOES DO MESMO TAMANHO-->
-
+</div>
 
 
 </body>
