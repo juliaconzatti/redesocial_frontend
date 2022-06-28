5aae95dbb96a1a1d10b4f8f20727c9fbf234b97f
Julia Conzatti <julia.140904@bento.local>
2022-06-28 16:21:14 -0300

    mudei o nome de inputs

--- atualizarLeitura.php
+++ atualizarLeitura.php
@@ -48,7 +48,7 @@
             <div class="row g-5">
               <h3 class="text-center">Histórico de leitura</h3>
               <input type="number" class="form-control" id="paginaslidas" placeholder="Número de páginas lidas">
-              <textarea class="form-control" id="textareaAtualizacao" rows="7" placeholder="Escreva o que está achando do livro aqui"></textarea><br>
+              <textarea class="form-control" id="textAtualizacao" rows="7" placeholder="Escreva o que está achando do livro aqui"></textarea><br>
 </div>
             <div class="d-grid gap-2">
               <button class="btn btn-outline-dark" type="submit" name="pub_atualizacao" id="cadastrar">Publicar atualização</button>
--- resenha.php
+++ resenha.php
@@ -48,7 +48,7 @@
 <div class="row g-5">
     <h3 class="text-center">Faça sua resenha aqui!</h3>
   <input type="text" class="form-control" id="tituloresenha" placeholder="Título da resenha">
-  <textarea class="form-control" id="textareaResenha" rows="7" placeholder="Escreva aqui a sua opinião sobre o livro"></textarea><br>
+  <textarea class="form-control" id="textResenha" rows="7" placeholder="Escreva aqui a sua opinião sobre o livro"></textarea><br>
   </div><br>
   <div class="d-grid gap-2">
                         <button class="btn btn-outline-dark" type="submit" name="pub_resenha"
